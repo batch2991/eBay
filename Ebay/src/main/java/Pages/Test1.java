b61fc7dc8ea8d80abdfeb30387f3eadf4bb73212
tester2 <tester2@gmail.com>
2021-07-19 08:47:38 +0530

    updated 1

--- Ebay/src/main/java/Pages/Test1.java
+++ Ebay/src/main/java/Pages/Test1.java
@@ -4,12 +4,13 @@ public class Test1
 {
   public void f1()
   {
-<<<<<<< HEAD
+
 	  System.out.println("hello");
 	  System.out.println("hello2");
 	  System.out.println("hello3");
-=======
-	  
->>>>>>> parent of 4f2b6c1 (updated)
+
+	  System.out.println("hello");
+	  System.out.println("hello2");
+	  System.out.println("hello3");
   }
 }
